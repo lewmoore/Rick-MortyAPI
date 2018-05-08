9d81d33aeb4f0a9f1c821096e0b24f0feb1e0a27
Lewis Moore <lewis.moore2707@gmail.com>
2018-05-08 15:12:49 +0100

    Add initial app rendering tests. Add enzyme config  and import dependencies to apptest

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../components/App';
+import Enzyme from 'enzyme'
+import { mount } from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16'
+
+Enzyme.configure({adapter: new EnzymeAdapter() });
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  div.id = 'root'
+  ReactDOM.render(<App />, div);;
 });
+
+it ('Renders welcome message', () => {
+  const page = mount(<App />);
+  expect(page.text()).toMatch('Hello World')
+})
