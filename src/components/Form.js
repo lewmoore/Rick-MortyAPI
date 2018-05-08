97def2fe2ec9ff6e92fc4c8a4d8bf089bc384489
Lewis Moore <lewis.moore2707@gmail.com>
2018-05-08 16:53:59 +0100

    Add handleChange and HandleSubmit of Form component

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,28 @@
 import React from 'react';
 
 class Form extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {value: ''}
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange(event){
+    this.setState({value: event.target.value})
+  }
+
+  handleSubmit(event){
+    console.log('Success' + this.state.value)
+    event.preventDefault();
+  }
+
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <label>Character
-        <input type='text' name='character' />
+        <input className='name' type='text' name='character' value={this.state.value} onChange={this.handleChange}/>
         <br/>
         </label>
         <input type='submit' value='search' />
--- src/tests/Form.test.js
+++ src/tests/Form.test.js
@@ -4,15 +4,35 @@ import Form from '../components/Form';
 import Enzyme from 'enzyme'
 import { mount, shallow } from 'enzyme';
 import EnzymeAdapter from 'enzyme-adapter-react-16'
+import Sinon from 'sinon';
 
 
 Enzyme.configure({adapter: new EnzymeAdapter() });
 
 describe('Form', () => {
 
-  const form = shallow(<Form />);
-
   it("renders correctly", () => {
+    const form = shallow(<Form />);
     expect(form).toMatchSnapshot();
   });
+
+  it('calls handleSubmit fn on click', () => {
+    const handleChangeSpy = Sinon.spy(Form.prototype, 'handleSubmit')
+    const wrapper = shallow(
+      <Form />
+    );
+    const event = { preventDefault() {}};
+    wrapper.find('form').simulate('submit', event)
+    expect(handleChangeSpy.calledOnce).toEqual(true)
+  })
+
+it('responds to a change in income', () => {
+    const handleChangeSpy = Sinon.spy(Form.prototype, 'handleChange');
+    const event = {target: {value: 'Rick'}};
+    const wrapper = shallow(
+      <Form />
+    );
+    wrapper.find('.name').simulate('change', event);
+    expect(handleChangeSpy.calledOnce).toEqual(true)
+  })
 })
